1797ff3b6e4b9eee28410b351c3db52cf7db008e
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2023-09-23 22:45:03 -0300

    Ejercicio 2 - Datos en variables

---
+++ src/Ejercicios/Ejercicio2.java
@@ -0,0 +1,14 @@
+package Ejercicios;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        String nombre = "Flavia", cumpleaños = "07 de enero", ciudad = "Crespo", ocupacion = "Administrativa", hobbie = "leer";
+        int edad = 33;
+
+        
+        System.out.println("Hola, me llamo " + nombre);
+        System.out.println("Tengo " + edad + " años y mi cumpleaños es el " + cumpleaños);
+        System.out.println("Vivo en la ciudad de " + ciudad + " trabajo actualmente como " + ocupacion);
+        System.out.println("En mi tiempo libre, me gusta " + hobbie);
+    }    
+}
