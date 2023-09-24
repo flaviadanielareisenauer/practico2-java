d41f5efc400f778d9f019b1a83db1973a186cbb4
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2023-09-23 22:00:07 -0300

    ejercicio 1 - variables numericas

---
+++ src/Ejercicios/Ejercicio1.java
@@ -0,0 +1,13 @@
+package Ejercicios;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int uno = 11;
+        int dos = uno;
+        
+        uno = 27;
+        
+        System.out.println("Valor de uno: " + uno);
+        System.out.println("Valor de dos: " + dos);
+    }    
+}
