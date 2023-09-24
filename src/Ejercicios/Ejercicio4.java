55b4f121f37b0cd713dcbef28ebad2f99ad2cc92
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2023-09-23 22:52:31 -0300

    Ejercicio 4 - suma de valores

---
+++ src/Ejercicios/Ejercicio4.java
@@ -0,0 +1,13 @@
+package Ejercicios;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        int num1, num2, resultado;
+        
+        num1 = 7;
+        num2 = 26;        
+        resultado = num1 + num2;
+        
+        System.out.println("El resultado de la suma es: " + resultado);        
+    }    
+}
