de32209d1a086126a16ccfa0fa30f51ae46c8bae
Flavia Daniela Reisenauer <85591614+flaviadanielareisenauer@users.noreply.github.com>
2023-10-17 23:14:56 -0300

    Update Ejercicio4.java

--- src/Ejercicios/Ejercicio4.java
+++ src/Ejercicios/Ejercicio4.java
@@ -1,3 +1,11 @@
+/*
+Ejercicio 4: Suma de valores
+4. Creá tres variables: num1, num2, resultado.
+5. Asignale un valor a num1 y num2.
+6. Asignale a resultado el valor de la suma entre num1 y num2.
+7. Mostrá por consola el valor de resultado.
+*/
+
 package Ejercicios;
 
 public class Ejercicio4 {
