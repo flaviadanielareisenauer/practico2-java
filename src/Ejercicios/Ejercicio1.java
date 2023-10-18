63dedb80930a3545445a66d5e5705416c126298b
Flavia Daniela Reisenauer <85591614+flaviadanielareisenauer@users.noreply.github.com>
2023-10-17 23:12:36 -0300

    Update Ejercicio1.java

--- src/Ejercicios/Ejercicio1.java
+++ src/Ejercicios/Ejercicio1.java
@@ -1,3 +1,11 @@
+/*
+Ejercicio 1: Variables numéricas
+1. Declará una variable llamada uno y asignale un número cualquiera.
+2. Declará una variable llamada dos y asignale como valor la variable uno.
+3. Cambiá el valor de la variable uno.
+Pensá antes de ver el resultado: ¿cuál es el valor de la variable dos?
+*/
+
 package Ejercicios;
 
 public class Ejercicio1 {
