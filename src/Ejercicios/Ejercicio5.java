000a2ae3514ff2b674fe59ca6398246a8feac795
Flavia Daniela Reisenauer <85591614+flaviadanielareisenauer@users.noreply.github.com>
2023-10-17 23:15:45 -0300

    Update Ejercicio5.java

--- src/Ejercicios/Ejercicio5.java
+++ src/Ejercicios/Ejercicio5.java
@@ -1,3 +1,15 @@
+/*
+Calculador de abastecimiento de por vida
+Ejercicio 5: ¿Cuántos snacks vas a comer por el resto de tu vida? ¡Averigualo!
+1. Almacená tu edad en una variable.
+2. Guardá tu edad máxima en otra variable.
+3. Declará una variable con el nombre de tu snack favorito.
+4. Estimá cuántos snacks vas a comer por día y guardalo como un número en una variable.
+5. Calculá cuántos snacks te quedan por comer en el resto de tu vida.
+6. Mostrá en la consola el resultado: "Vas a necesitar NN snacks para que te alcancen hasta los XX años.".
+7. Agregale un precio por unidad y descubrí cuánto vas a gastar en snacks el resto de tu vida.
+*/
+
 package Ejercicios;
 
 public class Ejercicio5 {
